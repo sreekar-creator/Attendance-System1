01ab97278b15c6f2ed670310fafd1b5ab37848a7
sreekar-creator <23eg110c60@anurag.edu.in>
2026-03-26 23:44:52 +0530

    Fix infinite routing loop

--- src/main/java/com/attendance/controller/IndexController.java
+++ src/main/java/com/attendance/controller/IndexController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    // Forward all non-API paths to the React index.html
-    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}" })
+    // Forward all non-API and non-file paths to the React index.html
+    @RequestMapping(value = { "/", "/{path:[^\\.]*}", "/{path:^(?!api$).*}/**/{subpath:[^\\.]*}" })
     public String getIndex() {
         return "forward:/index.html";
     }
